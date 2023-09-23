2583e5fb201458535371fe4a9795e9970c3bcc7a
Vadon <vadondaniel02@gmail.com>
2023-09-23 19:07:08 +0200

    Téma választó létrehozva

--- src/script.js
+++ src/script.js
@@ -4,18 +4,35 @@ const nextButton = document.getElementById('next');
 const answers = document.getElementById('answers');
 const question = document.getElementById('question');
 const questionBlock = document.getElementById('question-block');
+const topicBlock = document.getElementById('topic-block');
+const topicButtons = document.querySelectorAll('.topic');
 
 startButton.addEventListener('click', startQuiz);
 nextButton.addEventListener('click', nextQuestion);
 backButton.addEventListener('click', resetQuiz);
 
-let questionNumber = 0;
+topicButtons.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        questionTopic = event.target.dataset.topic;
+        topicButtons.forEach(function(topicButton) {
+            topicButton.classList.remove('selected');
+        })
+        button.classList.add('selected');
+        startButton.disabled = false;
+    });
+});
 
-import questions from './questions.json' assert { type: 'json' };
-console.log(questions);
+let questionNumber = 0;
+var questionTopic = "";
+var questions = {};
+
+import allQuestions from './questions.json' assert { type: 'json' };
 
 function startQuiz() {
     resetScreen();
+    questions = allQuestions.filter(function (question) {
+        return question.topic === questionTopic;
+      });
     shuffleQuestions(questions);
     questionNumber = 0;
     startButton.classList.add('hide');
@@ -40,11 +57,13 @@ function resetScreen() {
     nextButton.classList.add('hide');
     backButton.classList.add('hide');
     questionBlock.classList.add('hide');
+    topicBlock.classList.add('hide');
 }
 
 function resetQuiz(){
     resetScreen();
     startButton.classList.remove('hide');
+    topicBlock.classList.remove('hide');
 }
 
 function nextQuestion(){
